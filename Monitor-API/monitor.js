15707548529b112bb7ed5ee5cffbb46f7e741286
Fennema lucas <ld.fennema@gmail.com>
2020-12-01 20:37:30 -0300

    monitor funcando

--- Monitor-API/monitor.js
+++ Monitor-API/monitor.js
@@ -3,13 +3,33 @@ const app = express();
 const rp = require("request-promise");
 require('dotenv').config()
 
-var router = express.Router();
+class Monitor {
+  urlList;
+  servicesStatus;
+  urlDiscord;
+  constructor(){
+    this.urlList = JSON.parse(process.env.SERVICE_LIST_WIDTH_NAME);
+    this.servicesStatus=new Map();
+    this.urlDiscord =  process.env.SERVICE_LIST;
+  }
+  beginListen(){
+    /**
+     *  @Todo
+     */
+    this.urlList.map(([url,name]) => {
+      rp.get({uri: `${url}/ping`, qs: {}, json: true}).then(res => this.notifyDiscord(url,name,res.message)).catch(e=> this.notifyDiscord(url,name,'error'))
+    });
+    setInterval(()=>{this.beginListen()},15000);
+  }
+  notifyDiscord(url,name,value){
+   if(this.servicesStatus[url] != value){
+    this.servicesStatus[url] = value
+     const message= value === 'error' ? { message : `Hora:${new Date()}| el servicio ${name} ha dejado de funcionar`} : { message : `Hora:${new Date()}| el servicio ${name} ha vuelto a la normalidad`}
+    
+     /**@TODO probar con discord mas tarde */
+     //rp.post({uri: this.urlDiscord, qs:{}, json:true, body});
+   }
+  }
+}
 
-app.use("/api", router);
-app.use(function (req, res) {
-  res.status(404).json({ status: 404, errorCode: "RESOURCE_NOT_FOUND" });
-});
-
-
-const port = process.env.PORT || 3001;
-app.listen(port);
+new Monitor().beginListen();
